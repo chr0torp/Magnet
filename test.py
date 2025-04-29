a4c6177e0a84e3966366463769fb2526cec0f6b3
chr0torp <christian.torp@hotmail.com>
2025-04-29 17:30:25 +0200

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import keyboard
+
+print("Press 'q' to quit...") 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+
+try:
+    while True:
+    GPIO.output(17, GPIO.HIGH)
+    sleep(2.5)
+    GPIO.output(17, GPIO.LOW)
+    sleep(2.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
