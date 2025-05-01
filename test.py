f2d2ad0892efe9bce765ccfe0eac581514a5ec1a
chr0torp <christian.torp@hotmail.com>
2025-05-01 15:37:35 +0200

    nu

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
 import time
 import RPi.GPIO as GPIO
-import keyboard
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
 try:
-    while True:
-        time.sleep(0.5)
-        if keyboard.is_pressed('s'):
-            print("'s' pressed")
-            GPIO.output(17, GPIO.HIGH)
-            
-        if keyboard.is_pressed('q'):
-            print("'q' pressed")
-            GPIO.output(17, GPIO.LOW)
-            break
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(10.0)        
+    GPIO.output(17, GPIO.LOW)
 
-        time.sleep(0.01)
+    time.sleep(2.0)
 
 except KeyboardInterrupt:
     print("Ctrl+C pressed, exiting.")
